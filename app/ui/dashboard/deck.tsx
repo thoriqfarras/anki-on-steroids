37068a7d57d40956b527195e385c0609b73a73fd
thoriq farras <thoriqibrahimfarras@gmail.com>
2024-10-03 15:44:17 -0700

    setup + add rough dashboard ui

---
+++ app/ui/dashboard/deck.tsx
@@ -0,0 +1,15 @@
+import { TrashIcon } from '@heroicons/react/24/outline';
+
+TrashIcon;
+
+export default function Deck({ title, flashcardNum }) {
+  return (
+    <div className="bg-zinc-100 drop-shadow-lg p-4 flex s w-full rounded-lg hover:brightness-90 cursor-pointer">
+      <span className="font-bold">{title ? title : 'Deck Title'}</span>
+      <span className="ml-auto mr-4">
+        {flashcardNum ? flashcardNum : 'X flashcards'}
+      </span>
+      <TrashIcon className="h-6" />
+    </div>
+  );
+}
